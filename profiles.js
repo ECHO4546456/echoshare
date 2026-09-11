aae68508cddabccdd8401962f19dd5b8aae330fb
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 09:22:44 -0800

    Launch

--- profiles.js
+++ profiles.js
@@ -15,8 +15,8 @@ const ECHO_PROFILES = {
         tagline: "This is the very beginning of the Echo//Share network.",
         birthDate: "Not here.",
         joined: "September 10, 2026",
-        readers: "2,9K",
-        support: "689K",
+        readers: "2M",
+        support: "6B",
         status: "Offline.",
 
         theme: "purple",
@@ -50,30 +50,29 @@ const ECHO_PROFILES = {
                     "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are.",
                     "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES",
                     "That is all check the LOG'S Rec to get started with the upcoming update.",
-                    "For starter's, Search: Update_Logs",
                 ]
             },
 
             {
                 heading: "History",
                 paragraphs: [
-                    "No history behind UPDATEE.",
+                    "TNo history behind UPDATEE.",
             
                 ]
             }
         ]
     }
-UPDATE: {
-        name: "SSML ACADEMY  SEASON 5 Hallow Nights",
+
+     Update_Logs: {
+        name: "Update_Logs",
         id: "ES-UPDATE-001",
         image: "https://media.discordapp.net/attachments/1535460677525700659/1547992978239389816/8d60023cfb1439ae0c17e6f85620a2c3.png?ex=6aa5704e&is=6aa41ece&hm=4d5c7987fb5c64f3acb6861504499f3782ef4eea3947a8b6ae1b2f02e03083e6&=&format=webp&quality=lossless",
-        tagline: "Check this for Updates for the website.",
+        tagline: "Update_Logs of the Echo//Share network.",
         birthDate: "Not here.",
         joined: "September 10, 2026",
-        readers: "9K",
-        support: "980k",
-        status: "Offline.",
-         
+        readers: "2.9K",
+        support: "789K",
+        status: "Online.",
 
         theme: "red",
 
@@ -96,14 +95,23 @@ UPDATE: {
                 heading: "Overview",
                 paragraphs: [
                     "Yooo, Hello SSML Family. welcome to the side of the ssml. I know ya'll ass feel werid about using some of the website.",
-                   
+                    "This will be where you check all the updates and documents for Now on so if you're interested in using echo share go for it.",
+                    "WHO ARE WE?:",
+                    "We're apart of the ssml dev team. We're the owners of this choatic and true story of a war zone server",
+                    "If you made it this far into the game,then you already know how shit runs around here. But then again, welcome to another huge Update of this server",
+                    "WHAT WILL BE ADDED HERE?:",
+                    "Documents and history",
+                    "You'll be able to search up people's documents and learn about the people they are closed to or just wanna learn about You're family or peers.",
+                    "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are.",
+                    "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES",
+                    "That is all check the LOG'S Rec to get started with the upcoming update.",
                 ]
             },
 
             {
-                heading: "Release_DATE: Oct1 / 2AM / ENDS  AT: Nov1",
+                heading: "History",
                 paragraphs: [
-                    "No history behind Update_Logs.",
+                    "TNo history behind UPDATEE.",
             
                 ]
             }
