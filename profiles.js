fb4ab92a799aa3b01dc07f684f4113b311a0d4da
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 08:48:45 -0800

    Launcch

--- profiles.js
+++ profiles.js
@@ -42,23 +42,22 @@ const ECHO_PROFILES = {
                     "Yooo, Hello SSML Family. welcome to the side of the ssml. I know ya'll ass feel werid about using some of the website.",
                     "This will be where you check all the updates and documents for Now on so if you're interested in using echo share go for it.",
                     "WHO ARE WE?:",
-                    "We're apart of the ssml dev team. We're the owners of this choatic and true story of a war zone server"
-                    "If you made it this far into the game,then you already know how shit runs around here. But then again, welcome to another huge Update of this server"
-                    "WHAT WILL BE ADDED HERE?:"
-                    "Documents and history"
-                    "You'll be able to search up people's documents and learn about the people they are closed to or just wanna learn about You're family or peers."
-                    "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are."
-                    "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES"
-                    "That is all check the LOG'S Rec to get started with the upcoming update."
+                    "We're apart of the ssml dev team. We're the owners of this choatic and true story of a war zone server",
+                    "If you made it this far into the game,then you already know how shit runs around here. But then again, welcome to another huge Update of this server",
+                    "WHAT WILL BE ADDED HERE?:",
+                    "Documents and history",
+                    "You'll be able to search up people's documents and learn about the people they are closed to or just wanna learn about You're family or peers.",
+                    "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are.",
+                    "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES",
+                    "That is all check the LOG'S Rec to get started with the upcoming update.",
                 ]
             },
 
             {
                 heading: "History",
                 paragraphs: [
-                    "This is another long paragraph.",
-                    "Another paragraph goes here.",
-                    "Another paragraph goes here."
+                    "TNo history behind UPDATEE.",
+            
                 ]
             }
         ]
