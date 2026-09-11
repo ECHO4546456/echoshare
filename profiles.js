ce5aacfb7d4348bd48572e36e24491e0da79025f
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 09:10:45 -0800

    Launch

--- profiles.js
+++ profiles.js
@@ -64,7 +64,7 @@ const ECHO_PROFILES = {
         ]
     }
 Update_Logs: {
-        name: "SSML ACADEMY – SEASON 5 Hallow Nights 🎃👻",
+        name: "SSML ACADEMY  SEASON 5 Hallow Nights 🎃👻",
         id: "ES-UPDATE-001",
         image: "https://media.discordapp.net/attachments/1535460677525700659/1547992978239389816/8d60023cfb1439ae0c17e6f85620a2c3.png?ex=6aa5704e&is=6aa41ece&hm=4d5c7987fb5c64f3acb6861504499f3782ef4eea3947a8b6ae1b2f02e03083e6&=&format=webp&quality=lossless",
         tagline: "Check this for Updates for the website.",
@@ -82,7 +82,7 @@ Update_Logs: {
             "Frogmen",
             "Writer///Tea,",
             "Doc//Senders",
-            "ECHO RECORD",
+            "ECHO RECORD"
         ],
         articleTitle: "ABOUT THE UPDATE",
 
