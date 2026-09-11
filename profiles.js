bc4b98cc661be9e00ff721047cc56d6465ccc820
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 09:33:42 -0800

    Launch

--- profiles.js
+++ profiles.js
@@ -9,7 +9,7 @@
 
 const ECHO_PROFILES = {
     UPDATE: {
-        name: "UPDATE",
+        name: "ECHO//SHARE FIRST RELEASE DOC",
         id: "ES-UPDATE-001",
         image: "https://cdn.pfps.gg/pfps/3651-dark-purple-anime.png",
         tagline: "This is the very beginning of the Echo//Share network.",
@@ -56,7 +56,62 @@ const ECHO_PROFILES = {
             {
                 heading: "History",
                 paragraphs: [
-                    "TNo history behind UPDATEE.",
+                    "TNo history behind UPDATE.",
+            
+                ]
+            }
+        ]
+    }
+
+    Update_Logs: {
+        name: "Update_Logs",
+        id: "ES-UPDATE-002",
+        image: "https://cdn.pfps.gg/pfps/3651-dark-purple-anime.png",
+        tagline: "This is the very beginning of the Echo//Share network.",
+        birthDate: "Not here.",
+        joined: "September 10, 2026",
+        readers: "2M",
+        support: "6B",
+        status: "Offline.",
+
+        theme: "red",
+
+
+        Authors: [
+            "Frogmen",
+            "Writer///Tea,",
+            "Doc//Senders",
+            "ECHO RECORD"
+        ],
+        articleTitle: "ABOUT THE UPDATE",
+
+        /*
+            EDIT THIS ARTICLE YOURSELF.
+            Every letter is automatically wrapped by script.js so each
+            individual character can glow bright white + green on hover.
+        */
+        article: [
+            {
+                heading: "Overview",
+                paragraphs: [
+                    "Yooo, Hello SSML Family. welcome to the side of the ssml. I know ya'll ass feel werid about using some of the website.",
+                    "This will be where you check all the updates and documents for Now on so if you're interested in using echo share go for it.",
+                    "WHO ARE WE?:",
+                    "We're apart of the ssml dev team. We're the owners of this choatic and true story of a war zone server",
+                    "If you made it this far into the game,then you already know how shit runs around here. But then again, welcome to another huge Update of this server",
+                    "WHAT WILL BE ADDED HERE?:",
+                    "Documents and history",
+                    "You'll be able to search up people's documents and learn about the people they are closed to or just wanna learn about You're family or peers.",
+                    "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are.",
+                    "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES",
+                    "That is all check the LOG'S Rec to get started with the upcoming update."
+                ]
+            },
+
+            {
+                heading: "History",
+                paragraphs: [
+                    "TNo history behind UPDATE.",
             
                 ]
             }
