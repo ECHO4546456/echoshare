f61693ea5ef3d926a1dc9431004b1f94f3ab675c
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 06:20:48 -0800

    Launch

--- profiles.js
+++ profiles.js
@@ -32,46 +32,4 @@ const ECHO_PROFILES = {
             }
         ]
     }
-
     },
-
-    AnotherCharacter: {
-        name: "AnotherCharacter",
-        id: "ES-CHAR-002",
-        image: "https://media.discordapp.net/attachments/1535460677525700659/1547827601920499852/Z.png?ex=6aa4d649&is=6aa384c9&hm=193f3131ebd5c887dd176efd5420227802689ac9232e6238a53d5162be30489c&=&format=webp&quality=lossless",
-        tagline: "Description of the new character.",
-        birthDate: "January 12, 2005",
-        joined: "September 11, 2026",
-        readers: "850K",
-        support: "3B",
-
-        theme: "red",
-
-        tags: [
-            "CHARACTER",
-            "ARCHIVE",
-            "DOCUMENTED",
-            "LEADER👑"
-        ],
-
-          article: [
-            {
-                heading: "Overview",
-                paragraphs: [
-                    "The Lost Hour. In the old town of Oakhaven, Silas worked in a tiny shop filled with ticking clocks. He spent his days fixing gears and cleaning springs.",
-                    "He liked things that made sense. Every gear had a place. Every tick had a purpose.",
-                    "One rainy Tuesday, a stranger left an odd box on the counter. The box had no lid, no hinges, and no seams.",
-                ]
-            },
-
-            {
-                heading: "History",
-                paragraphs: [
-                    "This is another long paragraph.",
-                    "Another paragraph goes here.",
-                    "Another paragraph goes here."
-                ]
-            }
-        ]
-    }
-};
