3472d6211cf2fa1055a30577378705073c94dd79
ECHO4546456 <echoshare454645@gmail.com>
2026-09-11 09:08:48 -0800

    Launch

--- profiles.js
+++ profiles.js
@@ -15,8 +15,8 @@ const ECHO_PROFILES = {
         tagline: "This is the very beginning of the Echo//Share network.",
         birthDate: "Not here.",
         joined: "September 10, 2026",
-        readers: "2M",
-        support: "6B",
+        readers: "2,9K",
+        support: "689K",
         status: "Offline.",
 
         theme: "purple",
@@ -50,13 +50,60 @@ const ECHO_PROFILES = {
                     "Simple: You don't need to sign in or nothing. any new thing you see show up on the channels in the discord server you will be provided with a link, all you need to do is serach Up the name of that channel in search bar anmd it will show you who they are.",
                     "It's that esay, also a mindful thought: Not every doc is is in this website be mindful this is a new run site so. BE ON THE LOOK UP FOR UPDATES",
                     "That is all check the LOG'S Rec to get started with the upcoming update.",
+                    "For starter's, Search: Update_Logs",
                 ]
             },
 
             {
                 heading: "History",
                 paragraphs: [
-                    "TNo history behind UPDATEE.",
+                    "No history behind UPDATEE.",
+            
+                ]
+            }
+        ]
+    }
+Update_Logs: {
+        name: "SSML ACADEMY – SEASON 5 Hallow Nights 🎃👻",
+        id: "ES-UPDATE-001",
+        image: "https://media.discordapp.net/attachments/1535460677525700659/1547992978239389816/8d60023cfb1439ae0c17e6f85620a2c3.png?ex=6aa5704e&is=6aa41ece&hm=4d5c7987fb5c64f3acb6861504499f3782ef4eea3947a8b6ae1b2f02e03083e6&=&format=webp&quality=lossless",
+        tagline: "Check this for Updates for the website.",
+        birthDate: "Not here.",
+        joined: "September 10, 2026",
+        readers: "9K",
+        support: "980k",
+        status: "Offline.",
+         
+
+        theme: "red",
+
+
+        Authors: [
+            "Frogmen",
+            "Writer///Tea,",
+            "Doc//Senders",
+            "ECHO RECORD",
+        ],
+        articleTitle: "ABOUT THE UPDATE",
+
+        /*
+            EDIT THIS ARTICLE YOURSELF.
+            Every letter is automatically wrapped by script.js so each
+            individual character can glow bright white + green on hover.
+        */
+        article: [
+            {
+                heading: "Overview",
+                paragraphs: [
+                    "Yooo, Hello SSML Family. welcome to the side of the ssml. I know ya'll ass feel werid about using some of the website.",
+                   
+                ]
+            },
+
+            {
+                heading: "Release_DATE: Oct1 / 2AM / ENDS  AT: Nov1",
+                paragraphs: [
+                    "No history behind Update_Logs.",
             
                 ]
             }
